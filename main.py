a35fb73fa1d327fcdffad3157b214d522bf80cbd
Vishwajith Shettigar <76042077+Vishwajith-Shettigar@users.noreply.github.com>
2021-11-21 10:58:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from plyer import notification
 import requests
 import json
-
+import time
 def notifyme(title,message):
     message=str(message)
     notification.notify(
@@ -18,15 +18,20 @@ def getreq(url):
     r=requests.get(url)
     return r.text
 if __name__ == '__main__':
+
+        
+    while True:
+        data=getreq('https://disease.sh/v3/covid-19/all')
     
-    data=getreq('https://disease.sh/v3/covid-19/all')
-   
-    soup=json.loads(data)
-    
-    
-    for i in soup.items():
-    
-        notifyme(str(i[0]),i[1])
+        soup=json.loads(data)
+        
+        
+        for i in soup.items():
+        
+            notifyme(str(i[0]),i[1])
+            
+        time.sleep(3600)
+        
+       
         
     
-   
